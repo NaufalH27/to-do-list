8d1afa8475e324a2c9fffdef8af9bc269c507822
unknown <naufalhmuzakki@gmail.com>
2024-11-28 22:24:46 +0700

    menambahkan jackson library untuk utilitas JSON file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.uns.todolist {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires com.google.gson;
     opens org.uns.todolist to javafx.fxml;
     exports org.uns.todolist;
 }
