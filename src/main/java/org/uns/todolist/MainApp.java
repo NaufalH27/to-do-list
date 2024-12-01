3678be7133b2d4786fedc83e907aca2cf91311f4
unknown <naufalhmuzakki@gmail.com>
2024-12-01 09:59:47 +0700

    rewrite MainApp fungsinalitasnya tetap sama

--- src/main/java/org/uns/todolist/MainApp.java
+++ src/main/java/org/uns/todolist/MainApp.java
@@ -1,37 +1,32 @@
 package org.uns.todolist;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 
 public class MainApp extends Application {
-    private static Stage stage;
+    public static final String TITLE = "TO-Do List";
+    public static final String FXML = "primary";
 
     @Override
     public void start(@SuppressWarnings("exports") Stage s) throws IOException {
-        stage=s;
-        setRoot("primary","todo-List");
+        Stage stage = s;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/" + FXML + ".fxml"));
+        setRoot(fxmlLoader, stage);
     }
 
-    static void setRoot(String fxml) throws IOException {
-        setRoot(fxml,stage.getTitle());
-    }
-
-    static void setRoot(String fxml, String title) throws IOException {
-        Scene scene = new Scene(loadFXML(fxml));
-        stage.setTitle(title);
+    static void setRoot(FXMLLoader fxmlLoader, Stage stage) throws IOException {
+        fxmlLoader.setControllerFactory(param -> new FXMLController());
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/"+fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
 
