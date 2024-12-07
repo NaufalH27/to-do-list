49da44e2186e184d0db96fbd01f6a361434c72b6
unknown <naufalhmuzakki@gmail.com>
2024-12-07 13:36:31 +0700

    remove unusued filter method

--- src/main/java/org/uns/todolist/service/FilterMethod.java
+++ src/main/java/org/uns/todolist/service/FilterMethod.java
@@ -1,11 +1,9 @@
 package org.uns.todolist.service;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.uns.todolist.helper.DayHelper;
 import org.uns.todolist.models.Task;
 
 public class FilterMethod {
@@ -22,42 +20,6 @@ public class FilterMethod {
     public static List<Task> ByIncomplete(List<Task> tasks) {
         return tasks.stream()
                 .filter(task -> !task.getIsCompleted()) 
-                .collect(Collectors.toList());
-    }
-
-    public static List<Task> ByToday(List<Task> tasks) {
-        Date today = DayHelper.getDayToday();  
-        return tasks.stream()
-                .filter(task -> {
-                    Date deadline = task.getDeadline();
-                    return deadline != null && deadline.equals(today); 
-                })
-                .collect(Collectors.toList());
-    }
-
-    public static List<Task> ByPast(List<Task> tasks) {
-        Date today = DayHelper.getDayToday();  
-        return tasks.stream()
-                .filter(task -> {
-                    Date deadline = task.getDeadline();
-                    return deadline != null && deadline.before(today); 
-                })
-                .collect(Collectors.toList());
-    }
-
-    public static List<Task> ByNoDeadline(List<Task> tasks) {
-        return tasks.stream()
-                .filter(task -> task.getDeadline() == null) 
-                .collect(Collectors.toList());
-    }
-
-    public static List<Task> ByFuture(List<Task> tasks) {
-        Date today = DayHelper.getDayToday();  
-        return tasks.stream()
-                .filter(task -> {
-                    Date deadline = task.getDeadline();
-                    return deadline != null && deadline.after(today); 
-                })
                 .collect(Collectors.toList());
     }
 
